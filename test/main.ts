df1758247303907c2a05fec2250f06255e53f443
GuoBin <844922881@qq.com>
2019-07-31 21:27:33 +0800

    test connection

--- test/main.ts
+++ test/main.ts
@@ -38,6 +38,11 @@ window.onload = function () {
     console.log('停止');
     rtc.stop().then();
   }, false);
+  domList[4].addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+  });
 
   rtc.on('start', (data: any) => {
     console.log(data);
