5355f5e5b2d0df465718123499d4c6fd28a4a7e7
GuoBin <guobin201314@gmail.com>
2020-07-13 15:05:02 +0800

    feat(dev): config and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ const gulp = require("gulp")
 const rollup = require("rollup")
 const rollupTypescript = require("rollup-plugin-typescript2")
 const path = require("path")
-import pkg from "./package.json"
-
-console.log(pkg.version)
 
 const RTC_CLIENT = {
   name: "rtc-client",
@@ -12,6 +9,14 @@ const RTC_CLIENT = {
   entryFile: path.resolve(__dirname, "packages/rtc-client/src/index.ts"),
   bundleDir: path.resolve(__dirname, "packages/rtc-client/dist"),
   tsconfig: path.resolve(__dirname, "packages/rtc-client/tsconfig.json")
+}
+
+const NODE_SERVER = {
+  name: "node-server",
+  rootDir: path.resolve(__dirname, "templates/server/"),
+  entryFile: path.resolve(__dirname, "templates/server/src/main.ts"),
+  bundleDir: path.resolve(__dirname, "templates/server/build"),
+  tsconfig: path.resolve(__dirname, "templates/server/tsconfig.json")
 }
 
 gulp.task("build-client", async function () {
@@ -57,6 +62,25 @@ gulp.task("build-client", async function () {
     file: `${RTC_CLIENT.bundleDir}/${RTC_CLIENT.name}.esm.js`,
     format: "esm",
     name: `${RTC_CLIENT.name}`,
+    sourcemap: true
+  })
+})
+
+gulp.task("build-node", async function () {
+  const bundle = await rollup.rollup({
+    input: NODE_SERVER.entryFile,
+    plugins: [
+      rollupTypescript({
+        tsconfig: NODE_SERVER.tsconfig
+      })
+    ]
+  })
+
+  // For Node.js
+  await bundle.write({
+    file: `${NODE_SERVER.bundleDir}/${NODE_SERVER.name}.cjs.js`,
+    format: "cjs",
+    name: `${NODE_SERVER.name}`,
     sourcemap: true
   })
 })
