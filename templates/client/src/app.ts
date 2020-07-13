1356c60cb2a62d8c6305d0b6e49c31990f22eb4f
GuoBin <guobin201314@gmail.com>
2020-07-13 15:04:21 +0800

    feat(client): add web

--- templates/client/src/app.ts
+++ templates/client/src/app.ts
@@ -1,1 +1,114 @@
+import { RtcClient } from "../../../packages/rtc-client/src"
 
+(function () {
+  console.log("app start...")
+  const localVideo: HTMLVideoElement = document.querySelector("#local-video")
+  const remoteVideo: HTMLVideoElement = document.querySelector("#remote-video")
+  const localInfo = document.querySelector(".info")
+  const inputEl: HTMLInputElement = document.querySelector("#rtc-id")
+  const rtcClient = new RtcClient(localVideo)
+
+  let ws = new WebSocket("ws://localhost:8090")
+
+  ws.onmessage = function (msg) {
+    console.log(msg.data)
+    try {
+      const offer = JSON.parse(msg.data)
+      if (offer.name && offer.name !== inputEl.value) {
+        // 另一个用户的信息
+        console.log(offer.sdp)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  ws.onopen = function () {
+    console.log("open")
+  }
+  ws.onclose = function (ev) {
+    console.log("node socket closed: ", ev)
+  }
+
+  const configuration = {
+    iceServers: [
+      {
+        urls: [
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302"
+        ]
+      }
+    ],
+    iceCandidatePoolSize: 10
+  }
+
+  let peerConnection = null
+  let localStream = null
+  let remoteStream = null
+
+  /**
+   * 打开本地摄像头
+   */
+  function openLocal() {
+    console.log("openLocal")
+    rtcClient.openCamera().then((stream) => {
+      localInfo.innerHTML = `ID: ${stream.id}`
+      localStream = stream
+      localVideo.srcObject = stream
+      // 创建RTCPeerConnection
+      peerConnection = new RTCPeerConnection(configuration)
+      localStream.getTracks().forEach(track => {
+        peerConnection.addTrack(track, localStream)
+      })
+      peerConnection.createOffer().then(offer => {
+        return peerConnection.setLocalDescription(offer)
+      }).then(() => {
+        if (!ws) {
+          ws = new WebSocket("ws://localhost:8090")
+        }
+        if (ws) {
+          ws.send(JSON.stringify({
+            name: inputEl.value,
+            target: "admin2",
+            type: "video-offer",
+            sdp: peerConnection.localDescription
+          }))
+        } else {
+          console.error("WebSocket not connect")
+        }
+
+      }).catch(error => {
+        console.log(error)
+      })
+    })
+  }
+
+  function closeLocal() {
+    rtcClient.closeCamera()
+    console.log("closeLocal")
+  }
+
+  function openRemote() {
+    console.log("openRemote")
+    remoteStream = new MediaStream()
+    peerConnection.addEventListener("track", event => {
+      console.log("Got remote track:", event.streams[0])
+      event.streams[0].getTracks().forEach(track => {
+        console.log("Add a track to the remoteStream:", track)
+        remoteStream.addTrack(track)
+      })
+    })
+  }
+
+  function closeRemote() {
+    console.log("closeRemote")
+    ws.close(3009, "客户端主动关闭")
+  }
+
+  setFunc([openLocal, closeLocal, openRemote, closeRemote])
+})()
+
+function setFunc(func: Array<() => void>) {
+  for (const fn of func) {
+    ;(window as any)[fn.name] = fn
+  }
+}
---
+++ templates/server/src/socket.ts
@@ -0,0 +1,75 @@
+import WebSocket from "ws"
+
+function normalize(): number {
+  if (process.env.WS_PORT) {
+    const p = Number(process.env.WS_PORT)
+    if (p > 8080 && p < 10010) {
+      return p
+    }
+  }
+  return 8090
+}
+
+const port = normalize()
+
+export function createWS() {
+  const ws = new WebSocket.Server({
+    port,
+    perMessageDeflate: {
+      zlibDeflateOptions: {
+        chunkSize: 1024,
+        memLevel: 7,
+        level: 3
+      },
+      zlibInflateOptions: {
+        chunkSize: 10 * 1024
+      },
+      clientNoContextTakeover: true,
+      serverNoContextTakeover: true,
+      serverMaxWindowBits: 10,
+
+      concurrencyLimit: 10,
+      threshold: 1024
+    }
+  })
+
+  setInterval(() => {
+    ws.clients.forEach(c => {
+      c.send(JSON.stringify({
+        type: "alive",
+        data: "ws alive interval"
+      }))
+    })
+  }, 15000)
+
+  ws.on("connection", webSocket => {
+    console.log("wss: connection: ", webSocket.protocol)
+    webSocket.onmessage = function (event: WebSocket.MessageEvent) {
+      console.log("client send message: ", event.data)
+      // 转发offer
+      ws.clients.forEach(c => c.send(event.data))
+    }
+    webSocket.send(JSON.stringify({
+      type: "info",
+      data: "connection success"
+    }))
+  })
+
+  ws.on("headers", ev => {
+    console.log(ev)
+  })
+
+  ws.on("close", () => {
+    console.log("close")
+  })
+
+  ws.on("error", ev => {
+    console.log(ev)
+  })
+
+  ws.on("headers", ev => {
+    console.log(ev)
+  })
+
+  return {ws, port}
+}
