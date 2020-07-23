840d6dcd95fb9e4c2fb86a7d557aa10ae24a5c0f
GuoBin <guobin201314@gmail.com>
2020-07-23 16:24:37 +0800

    feat(rtc-server): room的基本实现
    加入，移除，通知

--- packages/rtc-server/src/core/RtcRoom.ts
+++ packages/rtc-server/src/core/RtcRoom.ts
@@ -1,3 +1,36 @@
+import { BaseRtcRoom } from "../../../common"
+import { RtcVisitor } from "./RtcVisitor"
+import * as uuid from "uuid"
 
-export class RtcRoom {
+export class RtcRoom implements BaseRtcRoom {
+  public readonly id: string
+  public visitors: RtcVisitor[] = []
+
+  constructor() {
+    this.id = uuid.v4()
+  }
+
+  public addVisitor(v: RtcVisitor): void {
+    if (v.roomId === this.id) {
+      console.info(`${v} already in the room`)
+    } else {
+      v.roomId = this.id
+      this.visitors.push(v)
+    }
+  }
+
+  public broadcast(message: string): void {
+    this.visitors.forEach(v => v.send(message))
+  }
+
+  public notify(message: string, target: RtcVisitor[]): void {
+    target.forEach(v => v.send(message))
+  }
+
+  public removeVisitor(v: RtcVisitor): void {
+    const index = this.visitors.indexOf(v)
+    if (index >= 0) {
+      this.visitors.splice(index, 1)
+    }
+  }
 }
