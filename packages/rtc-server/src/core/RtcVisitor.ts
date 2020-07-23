5471ccf8cfebec6f395efadacb64c3c2307b70b9
GuoBin <guobin201314@gmail.com>
2020-07-23 16:24:56 +0800

    feat(rtc-server): visitor
    消息通知

--- packages/rtc-server/src/core/RtcVisitor.ts
+++ packages/rtc-server/src/core/RtcVisitor.ts
@@ -1,12 +1,33 @@
 import { BaseRtcVisitor } from "../../../common"
+import WebSocket from "ws"
 import * as uuid from "uuid"
 
 export class RtcVisitor implements BaseRtcVisitor {
   public readonly id: string
   public roomId: string
 
-  constructor() {
+  constructor(private wss: WebSocket) {
     this.id = uuid.v4()
     this.roomId = uuid.v4()
+  }
+
+  send(data: string) {
+    this._send(data)
+  }
+
+  sendBuffer(data: ArrayBuffer | ArrayBufferView) {
+    this._send(data)
+  }
+
+  sendBlob(data: Blob) {
+    this._send(data)
+  }
+
+  private _send(data: string | ArrayBufferLike | Blob | ArrayBufferView) {
+    try {
+      this.wss.send(data)
+    } catch (e) {
+      console.error(`RtcVisitor _send(data) error: ${e}`)
+    }
   }
 }
