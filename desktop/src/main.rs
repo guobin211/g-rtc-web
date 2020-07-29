36265b0f1cf18ecab327ba9f82c31a67285d999e
GuoBin <guobin201314@gmail.com>
2020-07-29 15:04:25 +0800

    rust string

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -6,6 +6,9 @@ use std::fs;
 use std::io::Read;
 use web_view::Content;
 
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
 
 fn main() {
     let mut _file = fs::File::open("web/dist/index.html").expect("index.html not found");
@@ -13,12 +16,16 @@ fn main() {
     let _size = _file.read_to_string(&mut _contents).expect("read index.html failed");
     println!("{}", _contents);
 
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
+
     let _web = web_view::builder()
       .title("AppRTC")
       .content(Content::Html(_contents))
       .size(420, 720)
       .resizable(true)
-      .debug(false)
+      .debug(true)
       .user_data(())
       .invoke_handler(|_web_view, args| {
         println!("{}", args);
